2d4180c8d920e0ebd406f3d50d5ca8a8fecb98d7
Somya <somyabakshi5@gmail.com>
2022-06-19 18:25:25 +0530

    Courses added

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -38,7 +38,7 @@ function Landing() {
           </div>
         </div>
       </nav>
-      <div className="container mt-5">
+      <div className="container mt-5 text-center">
         <a
           href="/"
           name="home"
@@ -53,8 +53,58 @@ function Landing() {
             Join us at Uthrive and polish your skills with the <br />
             first class instructors.
           </h4>
-          <a href="tel:+91-8210529561" className="mt-3 px-1 py-2" style={{display : 'inline-block',textDecoration:'none',color:'#776DEB',border:'1px solid #776DEB',borderRadius:'.3rem'}}>Get Started</a>
         </a>
+        <a href="tel:+91-8210529561" className="mt-3 px-1 py-2" style={{display : 'inline-block',textDecoration:'none',color:'#776DEB',border:'1px solid #776DEB',borderRadius:'.3rem'}}>Get Started</a>
+        <div className="our-courses">
+        <a href='/' name='courses' style={{textDecoration:'none',color:'#F77E87'}}><h2>Our Courses</h2></a>
+        <div id="courses-cards">
+          <div className="course-card">
+            <h4>Uthrive Placement Prep Course</h4>
+            <p>
+              A platform where you can book your seat for any stores, clinics,
+              salons etc. without any hassle
+            </p>
+          </div>
+          <div className="course-card">
+            <h4>Uthrive free webinars</h4>
+            <p>
+              We ensure your safety by providing limited slots for a day to
+              avoid crowd in public places
+            </p>
+          </div>
+          <div className="course-card">
+            <h4>Uthrive English Speaking Course</h4>
+            <p>
+              With our services you will never be unaware of new openings in
+              your locality.
+            </p>
+          </div>
+        </div>
+        <div id="courses-cards">
+          <div className="course-card">
+            <h4>Uthrive Placement Prep Course</h4>
+            <p>
+              A platform where you can book your seat for any stores, clinics,
+              salons etc. without any hassle
+            </p>
+          </div>
+          <div className="course-card">
+            <h4>Uthrive free webinars</h4>
+            <p>
+              We ensure your safety by providing limited slots for a day to
+              avoid crowd in public places
+            </p>
+          </div>
+          <div className="course-card">
+            <h4>Uthrive English Speaking Course</h4>
+            <p>
+              With our services you will never be unaware of new openings in
+              your locality.
+            </p>
+          </div>
+        </div>
+
+      </div>
       </div>
     </div>
   );
