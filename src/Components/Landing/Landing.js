d9d4ab67c1c1fcfe6008ea95110f79509a54e666
Somya <somyabakshi5@gmail.com>
2022-06-22 02:48:09 +0530

    review section added

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import "./Landing.css";
 import Nobg from "../../assets/image/Nobg.png";
 import img from "../../assets/image/Group 7.png";
+import shirley from "../../assets/image/shirley.jpg";
+import theo from "../../assets/image/theo.jpg";
+import daniel from "../../assets/image/daniel.jpg";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 function Landing() {
   const [display, setdisplay] = useState("none");
@@ -56,7 +61,12 @@ function Landing() {
               first class instructors.
             </p>
           </a>
-          <img className='bodyimg' src={img} style={{ width: "20rem" }} alt="" />
+          <img
+            className="bodyimg"
+            src={img}
+            style={{ width: "20rem" }}
+            alt=""
+          />
         </div>
         <a
           href="tel:+91-8210529561"
@@ -135,9 +145,63 @@ function Landing() {
           </div>
         </div>
       </div>
+      <div className='our-courses'>
+        <a
+          href="/"
+          name="reviews"
+          style={{ textDecoration: "none", color: "#F77E87" }}
+        >
+        <h2>Reviews</h2>
+        </a>
+        <Carousel showArrows={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+        autoPlay={true}
+        interval={6100}>
+        <div>
+          <img src={shirley} alt="" />
+          <div className="myCarousel">
+            <h3>Shirley Fultz</h3>
+            <h4>Designer</h4>
+            <p>
+              It's freeing to be able to catch up on customized news and not be
+              distracted by a social media element on the same site
+            </p>
+          </div>
+        </div>
+        <div>
+          <img src={daniel} alt="" />
+          <div className="myCarousel">
+            <h3>Daniel Keystone</h3>
+            <h4>Designer</h4>
+            <p>
+              The simple and intuitive design makes it easy for me use. I highly
+              recommend Fetch to my peers.
+            </p>
+          </div>
+        </div>
+        <div>
+          <img src={theo} alt="" />
+          <div className="myCarousel">
+            <h3>Theo Sorel</h3>
+            <h4>Designer</h4>
+            <p>
+              I enjoy catching up with Fetch on my laptop, or on my phone when
+              I'm on the go!
+            </p>
+          </div>
+        </div>
+      </Carousel>
+      </div>
       <footer className="container p-3 bg-light text-secondary">
-      Copyright &copy; 2020 Uthrive. All rights reserved.
-      Website Created by <a className="text-secondary"href="https://www.linkedin.com/in/somya-1231/">Somya</a>
+        Copyright &copy; 2020 Uthrive. All rights reserved. Website Created by{" "}
+        <a
+          className="text-secondary"
+          href="https://www.linkedin.com/in/somya-1231/"
+        >
+          Somya
+        </a>
       </footer>
     </div>
   );
