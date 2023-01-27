14a9d84344871baa6928adfdd2d39c17732efc88
Somya <somyabakshi5@gmail.com>
2023-01-28 05:18:08 +0530

    hulu

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -7,9 +7,11 @@ import design1 from "../../assets/images/Group 11.png";
 import imgforphone from "../../assets/images/imgforphone.png";
 import CourseCard from "../CourseCard/CourseCard";
 import College from "../College/College";
-import design2 from "../../assets/images/Design2.png"
-import design3 from "../../assets/images/Group 40.png"
+import design2 from "../../assets/images/Design2.png";
+import design3 from "../../assets/images/Group 40.png";
 import ReviewCard from "../ReviewCard/ReviewCard";
+import footerlogo from "../../assets/images/Ellipse 14.png";
+import birdie from "../../assets/images/Vector 27.png";
 
 function Landing() {
   const [clicked, setclicked] = useState(false);
@@ -76,30 +78,71 @@ function Landing() {
       </div>
       {/* our courses page */}
       <div id="courses">
-      <img src={design2} id="design2" alt="" />
-      <img src={design3} id="design3" alt="" />
+        <img src={design2} id="design2" alt="" />
+        <img src={design3} id="design3" alt="" />
         <h1>Our Courses</h1>
-        <CourseCard/>
+        <CourseCard />
       </div>
       <div id="colleges">
-      <div id="underlay">
-      <div id="overlay">
-        <h1>Colleges we have<br/>covered so far</h1>
-        <p>We have organised serveral webinars and have provided multiple<br/>
-placement prep courses in more than 20 colleges</p>
-<button>Know More <i className="ri-arrow-down-line"></i></button>
-      </div>
-      </div>
-      <College/>
+        <div id="underlay">
+          <div id="overlay">
+            <h1>
+              Colleges we have
+              <br />
+              covered so far
+            </h1>
+            <p>
+              We have organised serveral webinars and have provided multiple
+              <br />
+              placement prep courses in more than 20 colleges
+            </p>
+            <button>
+              Know More <i className="ri-arrow-down-line"></i>
+            </button>
+          </div>
+        </div>
+        <College />
       </div>
       <div id="testimonials">
-      <h1>What Our Students Say</h1>
+        <h1>What Our Students Say</h1>
         <img src={design2} id="design4" alt="" />
         <div id="review-card">
-          <ReviewCard/>
+          <ReviewCard />
         </div>
-
       </div>
+      <footer>
+        <div className="cols">
+          <img id="futerlogo" src={footerlogo} alt="" />
+          <p id="quote">
+            A little progress each
+            <br />
+            day adds up to great results
+          </p>
+          <div id="follow-div">
+          <img src={birdie} id="follow-bird" alt="" />
+          <p className="custom-footer-styling" id="follow">Follow Us</p>
+          </div>
+          <div className="logos">
+            <a href="#home"><i className="ri-facebook-circle-fill"></i></a>
+            <a href="#home"><i className="ri-instagram-fill"></i></a>
+            <a href="#home"><i className="ri-linkedin-box-fill"></i></a>
+            <a href="#home"><i className="ri-whatsapp-fill"></i></a>
+          </div>
+        </div>
+        <div className="cols">
+        <div id="quicklinks-div">
+          <img src={birdie} id="quicklinks-bird" alt="" />
+          <p className="custom-footer-styling" id="quicklinks">Quick Links</p>
+          </div>
+          <div id="footer-links">
+          <a href="#home">Home</a>
+            <a href="#home">Our Courses</a>
+            <a href="#home">Reviews</a>
+            <a href="#home">About</a>
+          </div>
+        </div>
+        <div className="cols"></div>
+      </footer>
     </div>
   );
 }
